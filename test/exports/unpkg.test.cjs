75de757e83bd6918f40aeb9bd4283438a1f4557f
Kevin Malakoff <kmalakoff@gmail.com>
2022-06-27 00:12:07 -0700

    Update ts-dev-stack

--- test/exports/unpkg.test.cjs
+++ test/exports/unpkg.test.cjs
@@ -1,10 +1,10 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const assert = require("assert");
-const indexOfNewline = require("index-of-newline/dist/umd/index-of-newline.js");
+const assert = require('assert');
+const indexOfNewline = require('index-of-newline/dist/umd/index-of-newline.js');
 
-describe("exports index-of-newline/dist/umd/index-of-newline.js", function () {
-  it("first newline", function () {
-    const index = indexOfNewline("some\r\nstring\ncombination\r");
+describe('exports index-of-newline/dist/umd/index-of-newline.js', function () {
+  it('first newline', function () {
+    const index = indexOfNewline('some\r\nstring\ncombination\r');
     assert.equal(index, 4);
   });
 });
