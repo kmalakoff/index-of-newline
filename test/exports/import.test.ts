5b0dcf9ce7de2d155d32b4f340073bc9a8d4e698
Kevin Malakoff <kmalakoff@gmail.com>
2024-02-15 17:21:21 -0800

    Upgrade to ts-dev-stack

--- test/exports/import.test.ts
+++ test/exports/import.test.ts
@@ -1,8 +1,8 @@
 import assert from 'assert';
 import indexOfNewline from 'index-of-newline';
 
-describe('exports .ts', function () {
-  it('first newline', function () {
+describe('exports .ts', () => {
+  it('first newline', () => {
     const index = indexOfNewline('some\r\nstring\ncombination\r');
     assert.equal(index, 4);
   });
