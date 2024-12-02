fbd00a7c512ecbe87f8b42c8f5b792ec449998c2
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-02 06:09:19 -0800

    Upgrade modules

--- test/exports/require.test.cjs
+++ test/exports/require.test.cjs
@@ -1,9 +1,8 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 const assert = require('assert');
 const indexOfNewline = require('index-of-newline');
 
-describe('exports .cjs', function () {
-  it('first newline', function () {
+describe('exports .cjs', () => {
+  it('first newline', () => {
     const index = indexOfNewline('some\r\nstring\ncombination\r');
     assert.equal(index, 4);
   });
