602364602d613f3652884088a8e0afe554936aaf
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-03 07:58:26 -0800

    Upgrade modules

--- dist/umd/index-of-newline.js
+++ dist/umd/index-of-newline.js
@@ -13,18 +13,18 @@
    * @returns When includeLength is true, returns a pair of [offset, length] to provide the length of CR (1), LF (1) or CRLF (2)
    */ function indexOfNewline(string) {
       var offset = arguments.length > 1 && arguments[1] !== void 0 ? arguments[1] : 0, includeLength = arguments.length > 2 && arguments[2] !== void 0 ? arguments[2] : false;
-      if (offset < 0) throw new Error("Unexpected negative offset");
+      if (offset < 0) throw new Error('Unexpected negative offset');
       if (offset > string.length) throw new Error("Offset is longer than the string. Offset: ".concat(offset, ". String: ").concat(string.length));
       while(offset < string.length){
           var value = string[offset];
-          if (value === "\n") return includeLength ? [
+          if (value === '\n') return includeLength ? [
               offset,
               1
           ] : offset;
-          else if (value === "\r") {
+          if (value === '\r') {
               return includeLength ? [
                   offset,
-                  string[offset + 1] === "\n" ? 2 : 1
+                  string[offset + 1] === '\n' ? 2 : 1
               ] : offset;
           }
           offset++;
