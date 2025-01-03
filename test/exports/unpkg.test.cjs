1f640d9cf05f0311cbc98cc4d04cd783cb5ee885
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-03 14:52:29 -0800

    Upgrade modules

--- test/exports/unpkg.test.cjs
+++ test/exports/unpkg.test.cjs
@@ -1,9 +1,8 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 const assert = require('assert');
-const indexOfNewline = require('index-of-newline/dist/umd/index-of-newline.js');
+const indexOfNewline = require('index-of-newline/dist/umd/index-of-newline.cjs');
 
-describe('exports index-of-newline/dist/umd/index-of-newline.js', function () {
-  it('first newline', function () {
+describe('exports index-of-newline/dist/umd/index-of-newline.cjs', () => {
+  it('first newline', () => {
     const index = indexOfNewline('some\r\nstring\ncombination\r');
     assert.equal(index, 4);
   });
