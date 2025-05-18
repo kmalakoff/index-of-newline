f382a8c1d7b6e017abbf1871e50d3fe3558206dd
Kevin Malakoff <kmalakoff@gmail.com>
2025-05-17 17:47:56 -0700

    Upgrade modules

--- test/exports/umd.min.test.cjs
+++
@@ -1,9 +0,0 @@
-const assert = require('assert');
-const indexOfNewline = require('index-of-newline/dist/umd/index-of-newline.min.cjs');
-
-describe('exports index-of-newline/dist/umd/index-of-newline.min.cjs', () => {
-  it('first newline', () => {
-    const index = indexOfNewline('some\r\nstring\ncombination\r');
-    assert.equal(index, 4);
-  });
-});
